816ade37e69d899569abe1824507ac3b30923304
Taylor Stubbs <taylor.stubbs@infinityworks.com>
2020-09-11 10:43:48 +0100

    Server

--- Server/server.js
+++ Server/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+
 const app = express();
 
+app.use(express.static('./Source'));
 app.get('/reset', (req, res) => {
-    res.send()
+  res.send()
 });
 app.listen(8080, () => {
   console.log("Server loaded...")
